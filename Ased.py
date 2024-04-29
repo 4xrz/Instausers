634e0120db81916754cf24114e57112d3902c05e
alo <152735503+4xrz@users.noreply.github.com>
2024-04-29 09:57:18 +0300

    Ased.py

--- Ased.py
+++ Ased.py
@@ -10,7 +10,7 @@ i = Client('oao.hex')
 k = 'oao'
 get = i.get(k)
 
-bot = telebot.TeleBot('7076927531:AAGR-mKglzPScwJbDnMuKTQEtMcBEWvtAtA')
+bot = telebot.TeleBot('6844093093:AAFHEXSt1teojra5muR30eQOPPQf-FD6zoQ')
 
 
 jk = types.InlineKeyboardMarkup(row_width=3)
@@ -78,15 +78,36 @@ def sts(m):
 				loi = f'{a}_{f}.{e}'
 				lis = [li,lii,liii,liiii,lu,luu,lpp,lppp,lpppp,loi]
 				kkl = random.choice(lis)
-				h = check_insta_users(kkl)
-				
-				if h['Status'] =='available':
-					h8 = f'''
-				تم صيد يوزر جديد : `{kkl}` \n@j_tja | @AboMlakk ,
-				'''
-					bot.send_message(m.chat.id,text=f'				تم صيد يوزر جديد : @{kkl} \n@j_tja | @AboMlakk ,')
-				else:
-					fail = f'''
+				req = requests.post('https://www.instagram.com/api/v1/web/accounts/web_create_ajax/attempt/',headers ={'Host':'www.instagram.com',
+'content-length':'85',
+'sec-ch-ua':'" Not A;Brand";v="99", "Chromium";v="101"',
+'x-ig-app-id':'936619743392459',
+'x-ig-www-claim':'0',
+'sec-ch-ua-mobile':'?0',
+'x-instagram-ajax':'81f3a3c9dfe2',
+'content-type':'application/x-www-form-urlencoded',
+'accept':'*/*',
+'x-requested-with':'XMLHttpRequest',
+'x-asbd-id':'198387',
+'user-agent':generate_user_agent(),
+'x-csrftoken':'jzhjt4G11O37lW1aDFyFmy1K0yIEN9Qv',
+'sec-ch-ua-platform':'"Linux"',
+'origin':'https://www.instagram.com',
+'sec-fetch-site':'same-origin',
+'sec-fetch-mode':'cors',
+'sec-fetch-dest':'empty',
+'referer':'https://www.instagram.com/accounts/emailsignup/',
+'accept-encoding':'gzip, deflate, br',
+'accept-language':'en-IQ,en;q=0.9',
+'cookie':'csrftoken=jzhjt4G11O37lW1aDFyFmy1K0yIEN9Qv',
+'cookie':'mid=YtsQ1gABAAEszHB5wT9VqccwQIUL',
+'cookie':'ig_did=227CCCC2-3675-4A04-8DA5-BA3195B46425',
+'cookie':'ig_nrcb=1'},data=f'email=aakmnnsjskksmsnsn%40gmail.com&username={kkl}&first_name=&opt_into_one_tap=false')
+				if '{"message":"feedback_required","spam":true,"feedback_title":"Try Again Later","feedback_message":"We limit how often you can do certain things on Instagram to protect our community. Tell us if you think we made a mistake.","feedback_url":"repute/report_problem/scraping/","feedback_appeal_label":"Tell us","feedback_ignore_label":"OK","feedback_action":"report_problem","status":"fail"}' in req.text:
+					pass
+					print('⛔ USER IS BAND : '+kkl)
+				elif  '"errors": {"username":' in req.text or  '"code": "username_is_taken"' in req.text:
+				     fail = f'''
 				غير متاح {kkl} ❌
 				'''
 				#h = bot.send_message(m.chat.id,text='انجب؟')
@@ -95,6 +116,10 @@ def sts(m):
 				asi.add(oob)
 				sleep(2)
 				bot.edit_message_reply_markup(chat_id=ii.chat.id, message_id=ii.message_id,reply_markup=asi)
+				print('❌ USER IS UNVALID : '+kkl)
+			else:
+					print('✅ USER IS VALID : '+kkl)
+					bot.send_message(m.chat.id,text=f'				تم صيد يوزر جديد : @{kkl} \n@j_tja | @AboMlakk ,',parse_mode='markdown')
 			
 			'''
 	if data =='sb' and get==True:
